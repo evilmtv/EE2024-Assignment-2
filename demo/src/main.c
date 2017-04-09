dcde8c09604fde46edfaa73e9f2f8564455e5dcc
evilmtv <diaryofnotes@gmail.com>
2017-04-09 21:26:27 +0800

    improve blinking consistency, added refreshrate selector

--- demo/src/main.c
+++ demo/src/main.c
@@ -21,8 +21,9 @@
 #include "led7seg.h"
 #include "light.h"
 
-static uint8_t barPos = 2;
-
+//static uint8_t barPos = 2;
+int refreshRate = 1000;
+int8_t alertstatus = 0;
 static char* msg = NULL;
 
 volatile uint32_t msTicks; // counter for 1ms SysTicks
@@ -31,6 +32,30 @@ volatile uint32_t msTicks; // counter for 1ms SysTicks
 //  SysTick_Handler - just increment SysTick counter
 void SysTick_Handler(void) {
 	msTicks++;
+
+	// Blink LED to alert user accordingly
+	if (alertstatus == 1) {
+		if (msTicks % 333 == 0) {
+			rgb_setLeds(2); // Set LED to BLUE
+		}
+		if ((msTicks + 167) % 333 == 0) {
+			rgb_setLeds(0); // Turn off LEDs
+		}
+	} else if (alertstatus == 2) {
+		if (msTicks % 333 == 0) {
+			rgb_setLeds(1); // Set LED to BLUE
+		}
+		if ((msTicks + 167) % 333 == 0) {
+			rgb_setLeds(0); // Turn off LEDs
+		}
+	} else if (alertstatus == 3) {
+		if (msTicks % 333 == 0) {
+			rgb_setLeds(2); // Set LED to BLUE
+		}
+		if ((msTicks + 167) % 333 == 0) {
+			rgb_setLeds(1); // Turn off LEDs
+		}
+	}
 }
 
 int getTicks(void) {
@@ -48,22 +73,22 @@ __INLINE void systick_delay(uint32_t delayTicks) {
 		;
 }
 
-static void moveBar(uint8_t steps, uint8_t dir) {
-	uint16_t ledOn = 0;
-
-	if (barPos == 0)
-		ledOn = (1 << 0) | (3 << 14);
-	else if (barPos == 1)
-		ledOn = (3 << 0) | (1 << 15);
-	else
-		ledOn = 0x07 << (barPos - 2);
-
-	barPos += (dir * steps);
-	barPos = (barPos % 16);
-
-	pca9532_setLeds(ledOn, 0xffff);
-}
-
+//static void moveBar(uint8_t steps, uint8_t dir) {
+//	uint16_t ledOn = 0;
+//
+//	if (barPos == 0)
+//		ledOn = (1 << 0) | (3 << 14);
+//	else if (barPos == 1)
+//		ledOn = (3 << 0) | (1 << 15);
+//	else
+//		ledOn = 0x07 << (barPos - 2);
+//
+//	barPos += (dir * steps);
+//	barPos = (barPos % 16);
+//
+//	pca9532_setLeds(ledOn, 0xffff);
+//}
+//
 //static void drawOled(uint8_t joyState) {
 //	static int wait = 0;
 //	static uint8_t currX = 48;
@@ -103,122 +128,121 @@ static void moveBar(uint8_t steps, uint8_t dir) {
 //		lastY = currY;
 //	}
 //}
+//
+//#define NOTE_PIN_HIGH() GPIO_SetValue(0, 1<<26);
+//#define NOTE_PIN_LOW()  GPIO_ClearValue(0, 1<<26);
+//
+//static uint32_t notes[] = { 2272, // A - 440 Hz
+//		2024, // B - 494 Hz
+//		3816, // C - 262 Hz
+//		3401, // D - 294 Hz
+//		3030, // E - 330 Hz
+//		2865, // F - 349 Hz
+//		2551, // G - 392 Hz
+//		1136, // a - 880 Hz
+//		1012, // b - 988 Hz
+//		1912, // c - 523 Hz
+//		1703, // d - 587 Hz
+//		1517, // e - 659 Hz
+//		1432, // f - 698 Hz
+//		1275, // g - 784 Hz
+//		};
+//
+//static void playNote(uint32_t note, uint32_t durationMs) {
+//
+//	uint32_t t = 0;
+//
+//	if (note > 0) {
+//
+//		while (t < (durationMs * 1000)) {
+//			NOTE_PIN_HIGH()
+//			;
+//			Timer0_us_Wait(note / 2);
+//			//delay32Us(0, note / 2);
+//
+//			NOTE_PIN_LOW()
+//			;
+//			Timer0_us_Wait(note / 2);
+//			//delay32Us(0, note / 2);
+//
+//			t += note;
+//		}
+//
+//	} else {
+//		Timer0_Wait(durationMs);
+//		//delay32Ms(0, durationMs);
+//	}
+//}
+//
+//static uint32_t getNote(uint8_t ch) {
+//	if (ch >= 'A' && ch <= 'G')
+//		return notes[ch - 'A'];
+//
+//	if (ch >= 'a' && ch <= 'g')
+//		return notes[ch - 'a' + 7];
+//
+//	return 0;
+//}
+//
+//static uint32_t getDuration(uint8_t ch) {
+//	if (ch < '0' || ch > '9')
+//		return 400;
+//
+//	/* number of ms */
+//
+//	return (ch - '0') * 200;
+//}
+//
+//static uint32_t getPause(uint8_t ch) {
+//	switch (ch) {
+//	case '+':
+//		return 0;
+//	case ',':
+//		return 5;
+//	case '.':
+//		return 20;
+//	case '_':
+//		return 30;
+//	default:
+//		return 5;
+//	}
+//}
 
-#define NOTE_PIN_HIGH() GPIO_SetValue(0, 1<<26);
-#define NOTE_PIN_LOW()  GPIO_ClearValue(0, 1<<26);
+//static void playSong(uint8_t *song) {
+//	uint32_t note = 0;
+//	uint32_t dur = 0;
+//	uint32_t pause = 0;
+//
+//	/*
+//	 * A song is a collection of tones where each tone is
+//	 * a note, duration and pause, e.g.
+//	 *
+//	 * "E2,F4,"
+//	 */
+//
+//	while (*song != '\0') {
+//		note = getNote(*song++);
+//		if (*song == '\0')
+//			break;
+//		dur = getDuration(*song++);
+//		if (*song == '\0')
+//			break;
+//		pause = getPause(*song++);
+//
+//		playNote(note, dur);
+//		//delay32Ms(0, pause);
+//		Timer0_Wait(pause);
+//
+//	}
+//}
 
-static uint32_t notes[] = { 2272, // A - 440 Hz
-		2024, // B - 494 Hz
-		3816, // C - 262 Hz
-		3401, // D - 294 Hz
-		3030, // E - 330 Hz
-		2865, // F - 349 Hz
-		2551, // G - 392 Hz
-		1136, // a - 880 Hz
-		1012, // b - 988 Hz
-		1912, // c - 523 Hz
-		1703, // d - 587 Hz
-		1517, // e - 659 Hz
-		1432, // f - 698 Hz
-		1275, // g - 784 Hz
-		};
-
-static void playNote(uint32_t note, uint32_t durationMs) {
-
-	uint32_t t = 0;
-
-	if (note > 0) {
-
-		while (t < (durationMs * 1000)) {
-			NOTE_PIN_HIGH()
-			;
-			Timer0_us_Wait(note / 2);
-			//delay32Us(0, note / 2);
-
-			NOTE_PIN_LOW()
-			;
-			Timer0_us_Wait(note / 2);
-			//delay32Us(0, note / 2);
-
-			t += note;
-		}
-
-	} else {
-		Timer0_Wait(durationMs);
-		//delay32Ms(0, durationMs);
-	}
-}
-
-static uint32_t getNote(uint8_t ch) {
-	if (ch >= 'A' && ch <= 'G')
-		return notes[ch - 'A'];
-
-	if (ch >= 'a' && ch <= 'g')
-		return notes[ch - 'a' + 7];
-
-	return 0;
-}
-
-static uint32_t getDuration(uint8_t ch) {
-	if (ch < '0' || ch > '9')
-		return 400;
-
-	/* number of ms */
-
-	return (ch - '0') * 200;
-}
-
-static uint32_t getPause(uint8_t ch) {
-	switch (ch) {
-	case '+':
-		return 0;
-	case ',':
-		return 5;
-	case '.':
-		return 20;
-	case '_':
-		return 30;
-	default:
-		return 5;
-	}
-}
-
-static void playSong(uint8_t *song) {
-	uint32_t note = 0;
-	uint32_t dur = 0;
-	uint32_t pause = 0;
-
-	/*
-	 * A song is a collection of tones where each tone is
-	 * a note, duration and pause, e.g.
-	 *
-	 * "E2,F4,"
-	 */
-
-	while (*song != '\0') {
-		note = getNote(*song++);
-		if (*song == '\0')
-			break;
-		dur = getDuration(*song++);
-		if (*song == '\0')
-			break;
-		pause = getPause(*song++);
-
-		playNote(note, dur);
-		//delay32Ms(0, pause);
-		Timer0_Wait(pause);
-
-	}
-}
-
-static uint8_t * song = (uint8_t*) "C2.C2,D4,C4,F4,E8,";
+//static uint8_t * song = (uint8_t*) "C2.C2,D4,C4,F4,E8,";
 //(uint8_t*)"C2.C2,D4,C4,F4,E8,C2.C2,D4,C4,G4,F8,C2.C2,c4,A4,F4,E4,D4,A2.A2,H4,F4,G4,F8,";
 //"D4,B4,B4,A4,A4,G4,E4,D4.D2,E4,E4,A4,F4,D8.D4,d4,d4,c4,c4,B4,G4,E4.E2,F4,F4,A4,A4,G8,";
 
 static void init_ssp(void) {
 	SSP_CFG_Type SSP_ConfigStruct;
 	PINSEL_CFG_Type PinCfg;
-
 	/*
 	 * Initialize SPI pin connect
 	 * P0.7 - SCK;
@@ -240,27 +264,15 @@ static void init_ssp(void) {
 	PinCfg.Portnum = 2;
 	PinCfg.Pinnum = 2;
 	PINSEL_ConfigPin(&PinCfg);
-
-	PinCfg.Funcnum = 0;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-	PinCfg.Portnum = 1;
-	PinCfg.Pinnum = 31;
-	PINSEL_ConfigPin(&PinCfg);
-
 	SSP_ConfigStructInit(&SSP_ConfigStruct);
-
 	// Initialize SSP peripheral with parameter given in structure above
 	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
-
 	// Enable SSP peripheral
 	SSP_Cmd(LPC_SSP1, ENABLE);
-
 }
 
 static void init_i2c(void) {
 	PINSEL_CFG_Type PinCfg;
-
 	/* Initialize I2C2 pin connect */
 	PinCfg.Funcnum = 2;
 	PinCfg.Pinnum = 10;
@@ -268,16 +280,31 @@ static void init_i2c(void) {
 	PINSEL_ConfigPin(&PinCfg);
 	PinCfg.Pinnum = 11;
 	PINSEL_ConfigPin(&PinCfg);
-
 	// Initialize I2C peripheral
 	I2C_Init(LPC_I2C2, 100000);
-
 	/* Enable I2C1 operation */
 	I2C_Cmd(LPC_I2C2, ENABLE);
 }
 
 static void init_GPIO(void) {
-	// Initialize button
+	PINSEL_CFG_Type PinCfg;
+	// Initialize button SW4
+	PinCfg.Funcnum = 0;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 1;
+	PinCfg.Pinnum = 31;
+	PINSEL_ConfigPin(&PinCfg);
+	GPIO_SetDir(1, 1 << 31, 0);
+
+	//Initialize button SW3
+	PinCfg.Funcnum = 0;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 10;
+	PINSEL_ConfigPin(&PinCfg);
+	GPIO_SetDir(2, 1 << 10, 0);
 }
 
 void pinsel_uart3(void) {
@@ -304,29 +331,58 @@ void init_uart(void) {
 	UART_TxCmd(LPC_UART3, ENABLE);
 }
 
+void init_all() {
+	init_uart();
+	init_i2c();
+	light_init();
+	light_enable();
+	light_setRange(LIGHT_RANGE_4000);
+	init_ssp();
+	init_GPIO();
+	pca9532_init();
+	joystick_init();
+	acc_init();
+	oled_init();
+	led7seg_init();
+	temp_init(*getTicks);
+	rgb_init();
+
+	// Enable GPIO Interrupt P0.4 for SW3 (reset button)
+	LPC_GPIOINT ->IO0IntEnF |= 1 << 4;
+	NVIC_EnableIRQ(EINT3_IRQn);
+	// set up interrupt priority
+	NVIC_SetPriorityGrouping(5);
+	NVIC_SetPriority(EINT3_IRQn, (1 << (__NVIC_PRIO_BITS - 1))); // 10|000|000, first priority for external interrupt
+}
+
+void EINT3_IRQHandler(void) {
+	// SW3
+	if ((LPC_GPIOINT ->IO0IntStatF >> 4) & 0x1) {
+		LPC_GPIOINT ->IO0IntClr |= 1 << 4;
+		if (refreshRate == 1000) {
+			refreshRate = 1;
+			alertstatus = 0;
+		} else if (refreshRate == 1) {
+			refreshRate = 2000;
+			alertstatus = 1;
+		} else {
+			refreshRate = 1000;
+			alertstatus = 3;
+		}
+	}
+}
+
 int main(void) {
 
-	int monitor_state = 0;
-	int current_tick = 48;
-	int print_at_f_counter1 = 0;
-	int print_at_f_counter2 = 0;
-	int print_at_f_counter3 = 0;
-	int movement = 0;
-
-	int firedetected = 0;
-	int movingindark = 0;
-	int alertstatus = 0;
-
+	int8_t monitor_state = 0;
+	int8_t current_tick = 48;
+	int8_t print_at_f_counter1 = 0;
+	int8_t print_at_f_counter2 = 0;
+	int8_t print_at_f_counter3 = 0;
+	int8_t movement = 0;
+	int8_t firedetected = 0;
+	int8_t movingindark = 0;
 	uint32_t previousTime;
-	uint8_t data = 0;
-	uint32_t len = 0;
-	uint8_t line[64];
-
-	init_uart();
-	//test sending message
-	msg = "POWER ON.\r\n";
-	UART_Send(LPC_UART3, (uint8_t *) msg, strlen(msg), BLOCKING);
-
 	int32_t xoff = 0;
 	int32_t yoff = 0;
 	int32_t zoff = 0;
@@ -337,68 +393,39 @@ int main(void) {
 	int8_t yold = 0;
 	int8_t zold = 64;
 
-	uint8_t dir = 1;
-	uint8_t wait = 0;
+	init_all();
 
-	uint8_t state = 0;
-
-	uint8_t btn1 = 1;
-
-	init_i2c();
-	light_init();
-	light_enable();
-	light_setRange(LIGHT_RANGE_4000);
-	init_ssp();
-	init_GPIO();
-
-	pca9532_init();
-	joystick_init();
-	acc_init();
-	oled_init();
-	led7seg_init();
-	temp_init(*getTicks);
-	rgb_init();
-
-	/*
-	 * Assume base board in zero-g position when reading first value.
-	 */
+	/* ---- Calibrate ------> */
 	acc_read(&x, &y, &z);
 	xoff = 0 - x;
 	yoff = 0 - y;
 	zoff = 64 - z;
 
-	/* ---- Speaker ------> */
-
-	GPIO_SetDir(2, 1 << 0, 1);
-	GPIO_SetDir(2, 1 << 1, 1);
-
-	GPIO_SetDir(0, 1 << 27, 1);
-	GPIO_SetDir(0, 1 << 28, 1);
-	GPIO_SetDir(2, 1 << 13, 1);
-	GPIO_SetDir(0, 1 << 26, 1);
-
-	GPIO_ClearValue(0, 1 << 27); //LM4811-clk
-	GPIO_ClearValue(0, 1 << 28); //LM4811-up/dn
-	GPIO_ClearValue(2, 1 << 13); //LM4811-shutdn
-
-	/* <---- Speaker ------ */
+//	/* ---- Speaker ------> */
+//	GPIO_SetDir(2, 1 << 0, 1);
+//	GPIO_SetDir(2, 1 << 1, 1);
+//	GPIO_SetDir(0, 1 << 27, 1);
+//	GPIO_SetDir(0, 1 << 28, 1);
+//	GPIO_SetDir(2, 1 << 13, 1);
+//	GPIO_SetDir(0, 1 << 26, 1);
+//	GPIO_ClearValue(0, 1 << 27); //LM4811-clk
+//	GPIO_ClearValue(0, 1 << 28); //LM4811-up/dn
+//	GPIO_ClearValue(2, 1 << 13); //LM4811-shutdn
 
 	if (SysTick_Config(SystemCoreClock / 1000)) {
 		while (1)
 			;  // Capture error
 	}
 
-	moveBar(1, dir);
-
-	GPIO_SetDir(2, 1 << 1, 1);
-
-	GPIO_SetDir(1, 1 << 31, 0); //SW4
-
 	/* <---- Initialize ------ */
+	//test sending message
+	msg = "POWER ON.\r\n";
+	UART_Send(LPC_UART3, (uint8_t *) msg, strlen(msg), BLOCKING);
 	monitor_state = 0;
 	rgb_setLeds(0);
 	led7seg_setChar(32, FALSE);
 	oled_clearScreen(OLED_COLOR_BLACK);
+	previousTime = msTicks;
 
 	while (1) {
 		/* <---- State Manager ------ */
@@ -421,14 +448,14 @@ int main(void) {
 			firedetected = 0;
 			movingindark = 0;
 			alertstatus = 0;
-			systick_delay(1500);
+			refreshRate = 1000;
+			systick_delay(1000);
 		}
 
 		/* <---- Monitor Mode ------ */
 		if (monitor_state == 1) {
-
 			// Run once every second
-			while ((msTicks - previousTime) < 1000)
+			while ((msTicks - previousTime) < refreshRate)
 				;
 			previousTime = msTicks;	// read current tick counter
 
@@ -480,35 +507,35 @@ int main(void) {
 					alertstatus = 2;
 				}
 			}
-			// Blink LED to alert user accordingly
-			int blink_count = 0;
-			switch (alertstatus) {
-			case 1:
-				while (blink_count < 2) { // Blink 3 times
-					rgb_setLeds(2); // Set LED to BLUE
-					systick_delay(166);
-					rgb_setLeds(0); // Turn off LEDs
-					systick_delay(167);
-					blink_count++;
-				}
-			case 2:
-				while (blink_count < 2) { // Blink 3 times
-					rgb_setLeds(1); // Set LED to RED
-					systick_delay(166);
-					rgb_setLeds(0); // Turn off LEDs
-					systick_delay(167);
-					blink_count++;
-				}
-			case 3:
-				while (blink_count < 2) { // Blink 3 times
-					rgb_setLeds(1); // Set LED to RED
-					systick_delay(167);
-					rgb_setLeds(2); // Set LED to BLUE
-					systick_delay(166);
-					rgb_setLeds(0); // Turn off LEDs
-					blink_count++;
-				}
-			}
+//			// Blink LED to alert user accordingly
+//			int blink_count = 0;
+//			switch (alertstatus) {
+//			case 1:
+//				while (blink_count < 2) { // Blink 3 times
+//					rgb_setLeds(2); // Set LED to BLUE
+//					systick_delay(166);
+//					rgb_setLeds(0); // Turn off LEDs
+//					systick_delay(167);
+//					blink_count++;
+//				}
+//			case 2:
+//				while (blink_count < 2) { // Blink 3 times
+//					rgb_setLeds(1); // Set LED to RED
+//					systick_delay(166);
+//					rgb_setLeds(0); // Turn off LEDs
+//					systick_delay(167);
+//					blink_count++;
+//				}
+//			case 3:
+//				while (blink_count < 2) { // Blink 3 times
+//					rgb_setLeds(1); // Set LED to RED
+//					systick_delay(167);
+//					rgb_setLeds(2); // Set LED to BLUE
+//					systick_delay(166);
+//					rgb_setLeds(0); // Turn off LEDs
+//					blink_count++;
+//				}
+//			}
 
 //			if (y < 0) {
 //				dir = 1;
@@ -525,19 +552,27 @@ int main(void) {
 			/* <---- Update values on OLED ------ */
 			if (current_tick == 53 || current_tick == 65
 					|| current_tick == 70) {
-				char strLight[25], strTemp[25], strAcc[25];
+				oled_clearScreen(OLED_COLOR_BLACK);
+				char strLight[25], strTemp[25], strAcc[25], strRefresh[25];
 				sprintf(strLight, "Light: %d", lightval);
 				sprintf(strTemp, "Temp: %d", tempval);
 				sprintf(strAcc, "A: %d %d %d", x, y, z);
-
-				oled_clearScreen(OLED_COLOR_BLACK);
-				oled_putString(10, 10, "MONITOR", OLED_COLOR_WHITE,
+				if (refreshRate == 1) {
+					sprintf(strRefresh, "BOOST");
+					oled_putString(45, 10, strRefresh, OLED_COLOR_WHITE,
+							OLED_COLOR_BLACK);
+				} else if (refreshRate == 2000) {
+					sprintf(strRefresh, "ECO");
+					oled_putString(45, 10, strRefresh, OLED_COLOR_WHITE,
+							OLED_COLOR_BLACK);
+				}
+				oled_putString(1, 10, "MONITOR", OLED_COLOR_WHITE,
 						OLED_COLOR_BLACK);
-				oled_putString(10, 22, strTemp, OLED_COLOR_WHITE,
+				oled_putString(1, 22, strTemp, OLED_COLOR_WHITE,
 						OLED_COLOR_BLACK);
-				oled_putString(10, 34, strLight, OLED_COLOR_WHITE,
+				oled_putString(1, 34, strLight, OLED_COLOR_WHITE,
 						OLED_COLOR_BLACK);
-				oled_putString(10, 46, strAcc, OLED_COLOR_WHITE,
+				oled_putString(1, 46, strAcc, OLED_COLOR_WHITE,
 						OLED_COLOR_BLACK);
 			}
 
@@ -567,6 +602,7 @@ int main(void) {
 					print_at_f_counter1 = 0;
 					print_at_f_counter2++;
 					if (print_at_f_counter2 > 9) {
+						print_at_f_counter2 = 0;
 						print_at_f_counter3++;
 					}
 				}
