605e05febd4c182e82bf49de439835d25eb11b63
evilmtv <diaryofnotes@gmail.com>
2017-04-10 21:05:55 +0800

    clean code

--- demo/src/main.c
+++ demo/src/main.c
@@ -13,6 +13,9 @@
 #include "lpc17xx_timer.h"
 #include "lpc17xx_uart.h"
 
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 #include "joystick.h"
 #include "pca9532.h"
 #include "acc.h"
@@ -20,40 +23,38 @@
 #include "rgb.h"
 #include "led7seg.h"
 #include "light.h"
+#include "temp.h"
 
-//static uint8_t barPos = 2;
-int refreshRate = 1000;
-int8_t alertstatus = 0;
+int32_t refreshRate = 1000;
+int8_t alertStatus = 0;
 static char* msg = NULL;
-
 volatile uint32_t msTicks; // counter for 1ms SysTicks
 
-// ****************
-//  SysTick_Handler - just increment SysTick counter
+//  SysTick_Handler - increment SysTick counter
 void SysTick_Handler(void) {
 	msTicks++;
 
 	// Blink LED to alert user accordingly
-	if (alertstatus == 1) {
+	if (alertStatus == 1) {
 		if (msTicks % 333 == 0) {
 			rgb_setLeds(2); // Set LED to BLUE
 		}
 		if ((msTicks + 167) % 333 == 0) {
 			rgb_setLeds(0); // Turn off LEDs
 		}
-	} else if (alertstatus == 2) {
+	} else if (alertStatus == 2) {
 		if (msTicks % 333 == 0) {
-			rgb_setLeds(1); // Set LED to BLUE
+			rgb_setLeds(1); // Set LED to RED
 		}
 		if ((msTicks + 167) % 333 == 0) {
 			rgb_setLeds(0); // Turn off LEDs
 		}
-	} else if (alertstatus == 3) {
+	} else if (alertStatus == 3) {
 		if (msTicks % 333 == 0) {
 			rgb_setLeds(2); // Set LED to BLUE
 		}
 		if ((msTicks + 167) % 333 == 0) {
-			rgb_setLeds(1); // Turn off LEDs
+			rgb_setLeds(1); // Set LED to RED
 		}
 	}
 }
@@ -72,173 +73,6 @@ __INLINE void systick_delay(uint32_t delayTicks) {
 	while ((msTicks - currentTicks) < delayTicks)
 		;
 }
-
-//static void moveBar(uint8_t steps, uint8_t dir) {
-//	uint16_t ledOn = 0;
-//
-//	if (barPos == 0)
-//		ledOn = (1 << 0) | (3 << 14);
-//	else if (barPos == 1)
-//		ledOn = (3 << 0) | (1 << 15);
-//	else
-//		ledOn = 0x07 << (barPos - 2);
-//
-//	barPos += (dir * steps);
-//	barPos = (barPos % 16);
-//
-//	pca9532_setLeds(ledOn, 0xffff);
-//}
-//
-//static void drawOled(uint8_t joyState) {
-//	static int wait = 0;
-//	static uint8_t currX = 48;
-//	static uint8_t currY = 32;
-//	static uint8_t lastX = 0;
-//	static uint8_t lastY = 0;
-//
-//	if ((joyState & JOYSTICK_CENTER) != 0) {
-//		oled_clearScreen(OLED_COLOR_BLACK);
-//		return;
-//	}
-//
-//	if (wait++ < 3)
-//		return;
-//
-//	wait = 0;
-//
-//	if ((joyState & JOYSTICK_UP) != 0 && currY > 0) {
-//		currY--;
-//	}
-//
-//	if ((joyState & JOYSTICK_DOWN) != 0 && currY < OLED_DISPLAY_HEIGHT - 1) {
-//		currY++;
-//	}
-//
-//	if ((joyState & JOYSTICK_RIGHT) != 0 && currX < OLED_DISPLAY_WIDTH - 1) {
-//		currX++;
-//	}
-//
-//	if ((joyState & JOYSTICK_LEFT) != 0 && currX > 0) {
-//		currX--;
-//	}
-//
-//	if (lastX != currX || lastY != currY) {
-//		oled_putPixel(currX, currY, OLED_COLOR_WHITE);
-//		lastX = currX;
-//		lastY = currY;
-//	}
-//}
-//
-//#define NOTE_PIN_HIGH() GPIO_SetValue(0, 1<<26);
-//#define NOTE_PIN_LOW()  GPIO_ClearValue(0, 1<<26);
-//
-//static uint32_t notes[] = { 2272, // A - 440 Hz
-//		2024, // B - 494 Hz
-//		3816, // C - 262 Hz
-//		3401, // D - 294 Hz
-//		3030, // E - 330 Hz
-//		2865, // F - 349 Hz
-//		2551, // G - 392 Hz
-//		1136, // a - 880 Hz
-//		1012, // b - 988 Hz
-//		1912, // c - 523 Hz
-//		1703, // d - 587 Hz
-//		1517, // e - 659 Hz
-//		1432, // f - 698 Hz
-//		1275, // g - 784 Hz
-//		};
-//
-//static void playNote(uint32_t note, uint32_t durationMs) {
-//
-//	uint32_t t = 0;
-//
-//	if (note > 0) {
-//
-//		while (t < (durationMs * 1000)) {
-//			NOTE_PIN_HIGH()
-//			;
-//			Timer0_us_Wait(note / 2);
-//			//delay32Us(0, note / 2);
-//
-//			NOTE_PIN_LOW()
-//			;
-//			Timer0_us_Wait(note / 2);
-//			//delay32Us(0, note / 2);
-//
-//			t += note;
-//		}
-//
-//	} else {
-//		Timer0_Wait(durationMs);
-//		//delay32Ms(0, durationMs);
-//	}
-//}
-//
-//static uint32_t getNote(uint8_t ch) {
-//	if (ch >= 'A' && ch <= 'G')
-//		return notes[ch - 'A'];
-//
-//	if (ch >= 'a' && ch <= 'g')
-//		return notes[ch - 'a' + 7];
-//
-//	return 0;
-//}
-//
-//static uint32_t getDuration(uint8_t ch) {
-//	if (ch < '0' || ch > '9')
-//		return 400;
-//
-//	/* number of ms */
-//
-//	return (ch - '0') * 200;
-//}
-//
-//static uint32_t getPause(uint8_t ch) {
-//	switch (ch) {
-//	case '+':
-//		return 0;
-//	case ',':
-//		return 5;
-//	case '.':
-//		return 20;
-//	case '_':
-//		return 30;
-//	default:
-//		return 5;
-//	}
-//}
-
-//static void playSong(uint8_t *song) {
-//	uint32_t note = 0;
-//	uint32_t dur = 0;
-//	uint32_t pause = 0;
-//
-//	/*
-//	 * A song is a collection of tones where each tone is
-//	 * a note, duration and pause, e.g.
-//	 *
-//	 * "E2,F4,"
-//	 */
-//
-//	while (*song != '\0') {
-//		note = getNote(*song++);
-//		if (*song == '\0')
-//			break;
-//		dur = getDuration(*song++);
-//		if (*song == '\0')
-//			break;
-//		pause = getPause(*song++);
-//
-//		playNote(note, dur);
-//		//delay32Ms(0, pause);
-//		Timer0_Wait(pause);
-//
-//	}
-//}
-
-//static uint8_t * song = (uint8_t*) "C2.C2,D4,C4,F4,E8,";
-//(uint8_t*)"C2.C2,D4,C4,F4,E8,C2.C2,D4,C4,G4,F8,C2.C2,c4,A4,F4,E4,D4,A2.A2,H4,F4,G4,F8,";
-//"D4,B4,B4,A4,A4,G4,E4,D4.D2,E4,E4,A4,F4,D8.D4,d4,d4,c4,c4,B4,G4,E4.E2,F4,F4,A4,A4,G8,";
 
 static void init_ssp(void) {
 	SSP_CFG_Type SSP_ConfigStruct;
@@ -331,7 +165,7 @@ void init_uart(void) {
 	UART_TxCmd(LPC_UART3, ENABLE);
 }
 
-void init_all() {
+void initialize() {
 	init_uart();
 	init_i2c();
 	light_init();
@@ -371,14 +205,20 @@ void EINT3_IRQHandler(void) {
 
 int main(void) {
 
-	int8_t monitor_state = 0;
-	int8_t current_tick = 48;
+	/*
+	 * Singed (int), unsigned (uint), bits [range=2^x]
+	 * 8bit unsigned -> 0~255
+	 * 8bit signed -> -128~+127
+	 * "A good example is long. On one machine, it might be 32 bits (the minimum required by C). On another, it's 64 bits."
+	 */
+	int8_t monitorState = 0;
+	int8_t currentTick = 48;
 	int8_t print_at_f_counter1 = 0;
 	int8_t print_at_f_counter2 = 0;
 	int8_t print_at_f_counter3 = 0;
-	int8_t movement = 0;
-	int8_t firedetected = 0;
-	int8_t movingindark = 0;
+	int8_t movementDetected = 0;
+	int8_t fireDetected = 0;
+	int8_t movingInDarkDetected = 0;
 	uint32_t previousTime;
 	int32_t xoff = 0;
 	int32_t yoff = 0;
@@ -390,24 +230,13 @@ int main(void) {
 	int8_t yold = 0;
 	int8_t zold = 64;
 
-	init_all();
+	initialize();
 
 	/* ---- Calibrate ------> */
 	acc_read(&x, &y, &z);
 	xoff = 0 - x;
 	yoff = 0 - y;
 	zoff = 64 - z;
-
-//	/* ---- Speaker ------> */
-//	GPIO_SetDir(2, 1 << 0, 1);
-//	GPIO_SetDir(2, 1 << 1, 1);
-//	GPIO_SetDir(0, 1 << 27, 1);
-//	GPIO_SetDir(0, 1 << 28, 1);
-//	GPIO_SetDir(2, 1 << 13, 1);
-//	GPIO_SetDir(0, 1 << 26, 1);
-//	GPIO_ClearValue(0, 1 << 27); //LM4811-clk
-//	GPIO_ClearValue(0, 1 << 28); //LM4811-up/dn
-//	GPIO_ClearValue(2, 1 << 13); //LM4811-shutdn
 
 	if (SysTick_Config(SystemCoreClock / 1000)) {
 		while (1)
@@ -418,7 +247,7 @@ int main(void) {
 	//test sending message
 	msg = "POWER ON.\r\n";
 	UART_Send(LPC_UART3, (uint8_t *) msg, strlen(msg), BLOCKING);
-	monitor_state = 0;
+	monitorState = 0;
 	rgb_setLeds(0);
 	led7seg_setChar(32, FALSE);
 	oled_clearScreen(OLED_COLOR_BLACK);
@@ -426,43 +255,43 @@ int main(void) {
 
 	while (1) {
 		/* <---- State Manager ------ */
-		if (((GPIO_ReadValue(1) >> 31) & 0x01) == 0 && monitor_state == 0) {
+		if (((GPIO_ReadValue(1) >> 31) & 0x01) == 0 && monitorState == 0) {
 			msg = "Entering MONITOR Mode.\r\n";
 			UART_Send(LPC_UART3, (uint8_t *) msg, strlen(msg), BLOCKING);
-			monitor_state = 1;
+			monitorState = 1;
 			rgb_setLeds(4);
 			oled_clearScreen(OLED_COLOR_BLACK);
 			oled_putString(10, 10, "MONITOR", OLED_COLOR_WHITE,
 					OLED_COLOR_BLACK);
 		} else if (((GPIO_ReadValue(1) >> 31) & 0x01) == 0
-				&& monitor_state == 1) {
+				&& monitorState == 1) {
 			msg = "Entering Stable State.\r\n";
 			UART_Send(LPC_UART3, (uint8_t *) msg, strlen(msg), BLOCKING);
-			monitor_state = 0;
+			monitorState = 0;
 			rgb_setLeds(0);
 			led7seg_setChar(32, FALSE);
 			oled_clearScreen(OLED_COLOR_BLACK);
-			firedetected = 0;
-			movingindark = 0;
-			alertstatus = 0;
+			fireDetected = 0;
+			movingInDarkDetected = 0;
+			alertStatus = 0;
 			refreshRate = 1000;
 			systick_delay(1000);
 		}
 
 		/* <---- Monitor Mode ------ */
-		if (monitor_state == 1) {
+		if (monitorState == 1) {
 			// Run once every second
 			while ((msTicks - previousTime) < refreshRate)
 				;
 			previousTime = msTicks;	// read current tick counter
 
 			/* <---- Onboard timer shown on led7seg ------ */
-			led7seg_setChar(current_tick, FALSE);
-			current_tick++;
-			if (current_tick == 58) {
-				current_tick = 65;
-			} else if (current_tick == 71) {
-				current_tick = 48;
+			led7seg_setChar(currentTick, FALSE);
+			currentTick++;
+			if (currentTick == 58) {
+				currentTick = 65;
+			} else if (currentTick == 71) {
+				currentTick = 48;
 			}
 
 			/* <---- Update sensors values ------ */
@@ -477,9 +306,9 @@ int main(void) {
 			// Check for movement based on previous acc values
 			if (((abs(x - xold)) > 10) || ((abs(y - yold)) > 10)
 					|| ((abs(z - zold)) > 10)) {
-				movement = 1;
+				movementDetected = 1;
 			} else {
-				movement = 0;
+				movementDetected = 0;
 			}
 			xold = x;
 			yold = y;
@@ -487,68 +316,27 @@ int main(void) {
 
 			/* <---- LIGHT_LOW_WARNING & TEMP_HIGH_WARNING ------ */
 			// Check for user moving in dark places
-			if (movingindark == 0 && movement == 1 && lightval < 50) {
-				movingindark = 1;
-				if (firedetected == 1) {
-					alertstatus = 3;
+			if (movingInDarkDetected == 0 && movementDetected == 1
+					&& lightval < 50) {
+				movingInDarkDetected = 1;
+				if (fireDetected == 1) {
+					alertStatus = 3;
 				} else {
-					alertstatus = 1;
+					alertStatus = 1;
 				}
 			}
 			// Check if temp exceeds permissible limits
-			if (firedetected == 0 && tempval > 450) {
-				firedetected = 1;
-				if (movingindark == 1) {
-					alertstatus = 3;
+			if (fireDetected == 0 && tempval > 450) {
+				fireDetected = 1;
+				if (movingInDarkDetected == 1) {
+					alertStatus = 3;
 				} else {
-					alertstatus = 2;
+					alertStatus = 2;
 				}
 			}
-//			// Blink LED to alert user accordingly
-//			int blink_count = 0;
-//			switch (alertstatus) {
-//			case 1:
-//				while (blink_count < 2) { // Blink 3 times
-//					rgb_setLeds(2); // Set LED to BLUE
-//					systick_delay(166);
-//					rgb_setLeds(0); // Turn off LEDs
-//					systick_delay(167);
-//					blink_count++;
-//				}
-//			case 2:
-//				while (blink_count < 2) { // Blink 3 times
-//					rgb_setLeds(1); // Set LED to RED
-//					systick_delay(166);
-//					rgb_setLeds(0); // Turn off LEDs
-//					systick_delay(167);
-//					blink_count++;
-//				}
-//			case 3:
-//				while (blink_count < 2) { // Blink 3 times
-//					rgb_setLeds(1); // Set LED to RED
-//					systick_delay(167);
-//					rgb_setLeds(2); // Set LED to BLUE
-//					systick_delay(166);
-//					rgb_setLeds(0); // Turn off LEDs
-//					blink_count++;
-//				}
-//			}
-
-//			if (y < 0) {
-//				dir = 1;
-//				y = -y;
-//			} else {
-//				dir = -1;
-//			}
-//
-//			if (y > 1 && wait++ > (40 / (1 + (y / 10)))) {
-//				moveBar(1, dir);
-//				wait = 0;
-//		}
 
 			/* <---- Update values on OLED ------ */
-			if (current_tick == 53 || current_tick == 65
-					|| current_tick == 70) {
+			if (currentTick == 53 || currentTick == 65 || currentTick == 70) {
 				oled_clearScreen(OLED_COLOR_BLACK);
 				char strLight[25], strTemp[25], strAcc[25], strRefresh[25];
 				sprintf(strLight, "Light: %d", lightval);
@@ -574,14 +362,14 @@ int main(void) {
 			}
 
 			/* <---- Send message via UART ------ */
-			if (current_tick == 70) {
+			if (currentTick == 70) {
 				// Warning messages sent first
-				if (firedetected == 1) {
+				if (fireDetected == 1) {
 					char redmsg[30] = "Fire was Detected.\r\n";
 					UART_Send(LPC_UART3, (uint8_t *) redmsg, strlen(redmsg),
 							BLOCKING);
 				}
-				if (movingindark == 1) {
+				if (movingInDarkDetected == 1) {
 					char bluemsg[40] = "Movement in darkness was Detected.\r\n";
 					UART_Send(LPC_UART3, (uint8_t *) bluemsg, strlen(bluemsg),
 							BLOCKING);
@@ -604,20 +392,6 @@ int main(void) {
 					}
 				}
 			}
-
-			/* ####### Joystick and OLED  ###### */
-			/* # */
-
-//			state = joystick_read();
-//			if (state != 0)
-//				drawOled(state);
-			/* # */
-			/* ############################################# */
-
-			/*if (btn1 == 0)
-			 {
-			 playSong(song);
-			 } */
 
 			Timer0_Wait(1);
 		}
