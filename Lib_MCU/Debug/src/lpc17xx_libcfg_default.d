dee4f28ca081ab8bddc07d5a1f849a48774be50b
evilmtv <diaryofnotes@gmail.com>
2017-04-10 22:04:53 +0800

    misc

--- Lib_MCU/Debug/src/lpc17xx_libcfg_default.d
+++
@@ -1,7 +0,0 @@
-src/lpc17xx_libcfg_default.d: ../src/lpc17xx_libcfg_default.c \
- D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_MCU\inc/lpc17xx_libcfg_default.h \
- D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_MCU\inc/lpc_types.h
-
-D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_MCU\inc/lpc17xx_libcfg_default.h:
-
-D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_MCU\inc/lpc_types.h:
--- demo/Debug/src/cr_startup_lpc17.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
- D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
-
-D:\NUS\Year\ 2\ AY2016-2017\EE2024\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
